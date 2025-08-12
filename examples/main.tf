036550a2795dac244ca78d2f2cf6a18305cdd007
avison <avisonlivingstone@gmail.com>
2025-08-12 18:00:55 +0000

    run git push '

---
+++ examples/main.tf
@@ -0,0 +1,26 @@
+// Dagster module sets up the Dagster platform and its ECS services
+// It connects to the client’s database and uses network infrastructure configured by the networking module
+module "postgrest" {
+  source                          = "../postgrest"
+  environment                     = var.environment                     # Deployment environment
+  region                          = var.region
+  vpc_id                  = var.vpc_id
+  rds_endpoint            = var.rds_endpoint
+  rds_secret_arn          = var.rds_secret_arn
+  api_secret_arn          = var.api_secret_arn
+  az_count                = var.az_count
+  postgres_subnet_cidrs   = var.postgres_subnet_cidrs
+  availability_zones      = var.availability_zones
+  postgrest_service_count = var.postgrest_service_count
+  postgrest_task_counts   = var.postgrest_task_counts
+  mapper_cpu              = var.mapper_cpu
+  mapper_memory           = var.mapper_memory
+  postgrest_cpu           = var.postgrest_cpu
+  postgrest_memory        = var.postgrest_memory
+  log_retention_days      = var.log_retention_days
+  mapper_image_tag        = var.mapper_image_tag
+  postgrest_image_tag     = var.postgrest_image_tag
+  config_url              = var.config_url
+  meta_secret_arn         = var.meta_secret_arn
+  meta_hash_arn           = var.meta_hash_arn 
+}
---
+++ examples/output.tf
@@ -0,0 +1,35 @@
+# outputs.tf
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = module.postgrest.alb_dns_name
+}
+
+output "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = module.postgrest.ecs_cluster_name
+}
+
+output "mapper_service_name" {
+  description = "Name of the mapper service"
+  value       = module.postgrest.mapper_service_name
+}
+
+output "postgrest_service_names" {
+  description = "Names of the PostgREST services"
+  value       = module.postgrest.postgrest_service_names
+}
+
+output "postgres_subnet_ids" {
+  description = "IDs of the PostgreSQL subnets"
+  value       = module.postgrest.postgres_subnet_ids
+}
+
+output "rds_secret_arn" {
+  description = "ARN of the Secrets Manager secret for RDS credentials"
+  value       = module.postgrest.rds_secret_arn
+}
+
+output "api_secret_arn" {
+  description = "ARN of the Secrets Manager secret for API key"
+  value       = module.postgrest.api_secret_arn
+}
